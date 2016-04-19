f9d22298426cdf5abd5ca4791712ff8a5992539a
Casimodo72 <cazic@gmx.net>
2016-04-20 00:08:43 +0200

    Added FileContentResult2 and a missing file.

---
+++ src/Casimodo.Lib.Web/CustomOutputCacheAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace Casimodo.Lib.Web
+{
+    public class CustomOutputCacheAttribute : OutputCacheAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            if (Location == OutputCacheLocation.ServerAndClient)
+                filterContext.HttpContext.Response.Cache.SetOmitVaryStar(true);
+
+            base.OnResultExecuting(filterContext);
+        }
+    }
+}
---
+++ src/Casimodo.Lib.Web/FileContentResult.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Casimodo.Lib.Web
+{
+    /// <summary>
+    /// A FileContentResult that supports the "inline" content-disposition.
+    /// </summary>
+    public class FileContentResult2 : FileContentResult
+    {
+        public FileContentResult2(byte[] fileContent, string contentType, bool isInline, string fileName)
+            : base(fileContent, contentType)
+        {
+            IsInline = isInline;
+            FileDownloadName = fileName;
+        }
+
+        public bool IsInline { get; set; }
+
+        public string FileName { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            HttpResponseBase response = context.HttpContext.Response;
+            response.ContentType = ContentType;
+
+            if (!string.IsNullOrEmpty(FileDownloadName))
+            {
+                context.HttpContext.Response.AddHeader("Content-Disposition", new ContentDisposition
+                {
+                    FileName = FileDownloadName,
+                    Inline = IsInline
+                }.ToString());
+            }
+
+            WriteFile(response);
+        }
+    }
+}
