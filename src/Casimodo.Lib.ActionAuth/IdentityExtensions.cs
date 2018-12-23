b6049efe8d1582b9faeaab972e43a34b9979b2f0
Casimodo72 <cazic@gmx.net>
2018-12-23 13:47:41 +0100

    -

--- src/Casimodo.Lib.ActionAuth/IdentityExtensions.cs
+++ src/Casimodo.Lib.ActionAuth/IdentityExtensions.cs
@@ -13,5 +13,33 @@ namespace Casimodo.Lib.Auth
                 .Where(c => c.Type == ClaimTypes.Role)
                 .Select(c => c.Value);
         }
+
+#if (!NET_CORE)
+        public static bool IsInAnyRole(this IPrincipal user, params string[] roles)
+        {
+            if (roles == null || roles.Length == 0)
+                return false;
+
+            for (int i = 0; i < roles.Length; i++)
+                if (user.IsInRole(roles[i]))
+                    return true;
+
+            return false;
+
+            // NOTE: The approach below would also work.
+#if (false)
+            var claimRoles = ((ClaimsIdentity)user.Identity).FindAll(ClaimTypes.Role).Select(x => x.Value).ToArray();
+            if (claimRoles.Length == 0)
+                return false;
+
+            for (int i = 0; i < roles.Length; i++)
+                for (int j = 0; j < claimRoles.Length; j++)
+                    if (roles[i] == claimRoles[j])
+                        return true;
+
+            return false;
+#endif
+        }
+#endif
     }
 }
