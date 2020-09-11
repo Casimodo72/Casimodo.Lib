b84687a118b72842e1a5a1bf13b53cb1a8709216
Casimodo72 <cazic@gmx.net>
2020-09-11 11:16:18 +0200

    -

---
+++ src/Casimodo.Lib.Database.Standard/Models/PrecisionAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Casimodo.Lib.Data
+{
+    public class PrecisionAttribute : Attribute
+    {
+        public PrecisionAttribute(byte precision, byte scale)
+        {
+            Precision = precision;
+            Scale = scale;
+        }
+
+        public byte Precision { get; set; }
+
+        public byte Scale { get; set; }
+    }
+
+    // TODO: REVISIT: Apply manually if EF Core
+    /*
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Set default precision to decimal property
+        foreach (var property in modelBuilder.Model.GetEntityTypes()
+            .SelectMany(t => t.GetProperties())
+            .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
+        {
+            property.SetColumnType("decimal(18, 2)");
+        }
+    }
+    */
+}
