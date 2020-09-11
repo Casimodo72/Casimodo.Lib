a6779eb7de13aa083f7d11ea9d7572fcdbdcedde
Casimodo72 <cazic@gmx.net>
2020-09-11 12:13:33 +0200

    -

---
+++ src/Obsolete/Casimodo.Lib/Helpers/ImageHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Casimodo.Helpers
+{
+    // KABU TODO: Move to UI layer. Use a dedicated foreign open-source lib.
+    public static class ImageHelper
+    {
+        public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+            Graphics.FromImage(newImage).DrawImage(image, 0, 0, newWidth, newHeight);
+            return newImage;
+        }
+    }
+}
---
+++ src/Obsolete/Casimodo.Lib/Helpers/PropertyHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Casimodo.Lib.ComponentModel
+{
+    // KABU: TODO: Use a dedicated foreign open-source lib or perform refactoring.
+    public static class PropertyHelper
+    {
+        public static string PropertyName<TType, TProp>(this TType type, Expression<Func<TType, TProp>> expression)
+        {
+            if (expression == null)
+                throw new ArgumentNullException("expression");
+
+            return GetName<TType, TProp>(expression);
+        }
+
+        public static string GetName<TType, TProp>(Expression<Func<TType, TProp>> expression)
+        {
+            if (expression == null)
+                throw new ArgumentNullException("expression");
+
+            PropertyInfo prop = GetInfo<TType, TProp>(expression);
+            if (prop == null)
+                return null;
+
+            return prop.Name;
+        }
+
+        public static PropertyInfo GetInfo<TType, TProp>(Expression<Func<TType, TProp>> expression)
+        {
+            if (expression == null)
+                throw new ArgumentNullException("expression");
+
+            LambdaExpression lambda = expression as LambdaExpression;
+            MemberExpression memberExpression;
+            if (lambda.Body is UnaryExpression)
+            {
+                UnaryExpression unaryExpression = lambda.Body as UnaryExpression;
+                memberExpression = unaryExpression.Operand as MemberExpression;
+            }
+            else
+            {
+                memberExpression = lambda.Body as MemberExpression;
+            }
+            PropertyInfo propertyInfo = memberExpression.Member as PropertyInfo;
+
+            return propertyInfo;
+        }
+    }
+}
