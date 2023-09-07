cd41dc4d868ac579c71abb973a006f4858d6152a
Kasimier Buchcik <cazic@gmx.net>
2023-09-07 14:50:09 +0200

    -

--- src/Casimodo.Lib.ActionAuth/IdentityExtensions.cs
+++ src/Casimodo.Lib.ActionAuth/IdentityExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Security.Principal;
@@ -12,6 +14,19 @@ namespace Casimodo.Lib.Auth
             return ((ClaimsIdentity)identity).Claims
                 .Where(c => c.Type == ClaimTypes.Role)
                 .Select(c => c.Value);
+        }
+
+        public static string GetUserId(this IIdentity identity)
+        {
+            var userIdClaim = ((ClaimsIdentity)identity).Claims
+               .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            if (userIdClaim == null)
+            {
+                throw new InvalidOperationException("No user ID claim found in identity.");
+            }
+
+            return userIdClaim.Value;
         }
 
         public static bool IsInAnyRole(this IPrincipal user, params string[] roles)
